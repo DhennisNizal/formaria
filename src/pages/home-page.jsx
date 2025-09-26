6a437444d13380688dc4b07c7b5e0cab7d556e94
Dhennis Nizal <dhnizal@gmail.com>
2025-09-26 18:32:26 +0800

    Add about module in homepage

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PageLayout from "../components/page-layout";
 import Hero from "../modules/hero";
+import About from "../modules/about";
 
 const HomePage = () => {
   return (
     <PageLayout>
       <Hero />
+      <About />
     </PageLayout>
   );
 };
