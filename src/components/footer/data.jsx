4d2b3ef02b7a48911bed4b09ca64a880e7d327f5
Dhennis Nizal <dhnizal@gmail.com>
2025-09-28 15:55:40 +0800

    Add footer component with data and styles

---
+++ src/components/footer/data.jsx
@@ -0,0 +1,25 @@
+import { FaMapMarkerAlt, FaEnvelope, FaPhone } from "react-icons/fa";
+
+export const topContent = {
+  title: "Elevate your stay with Formaria",
+  description:
+    "At Formaria, we redefine comfort with elegant spaces, refined amenities, and service that makes every moment special. Your stay isn’t just elevated—it’s unforgettable.",
+};
+
+export const navLinks = [
+  { label: "Who we are" },
+  { label: "Our Rooms" },
+  { label: "Why choose us" },
+];
+
+export const contactInfo = [
+  { icon: <FaMapMarkerAlt />, text: "Manila, Philippines" },
+  { icon: <FaEnvelope />, text: "formaria@gmail.com" },
+  { icon: <FaPhone />, text: "09099199964" },
+];
+
+export const footerLogo = {
+  title: "Formaria",
+  developer: "Developed by Dhennis Nizal",
+  rights: "@2025 | All rights reserved",
+};
